51bd12bf8c1f3fbfb0e88b8fd5effbf1c4dea67c
Yash Kambaria <yashkambaria245@gmail.com>
2025-09-13 12:31:19 +0530

    Locks methods used

--- part3/BankAccount.java
+++ part3/BankAccount.java
@@ -1,25 +1,61 @@
 package part3;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 	private int balance=100;
 	
-	public synchronized void withdraw(int amount){
+	final private Lock lock=new ReentrantLock();
+	
+	/*
+	lock.tryLock(time,timeunit) will try to wait for the given time to
+	enter and acquire lock but if the time is more than that then it
+	does not executes
+	
+	but in lock.tryLock() it will only lock if the
+	lock is free otherwise it will not execute or wait
+	
+	and lock.lock() will wait till it does not get
+	the critical area free (same as synchronized)
+	
+	 */
+	
+	
+	public void withdraw(int amount){
+		try {
+			if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+				if (amount <= balance) {
+					try {
+						System.out.println(Thread.currentThread().getName() + " is processing payment");
+						Thread.sleep(1000);
+						balance -= amount;
+						System.out.println(Thread.currentThread().getName() + " completed transaction current balance " + balance);
+					} catch (Exception e) {
+						Thread.currentThread().interrupt();
+					
+					} finally {
+						lock.unlock();
+					}
+				} else {
+					System.out.println("Insufficient balance");
+				}
+			}
+			else {
+				System.out.println("Lock is acquired please try again later");
+			}
+		}
+		catch (Exception e){
+			//here we have used this to perform the following task -
+			Thread.currentThread().interrupt();                 // |
+		}                                                       // |
+		/*                                                      // |
+		if(Thread.currentThread().isInterrupted()){  <-------------|
+			perform login or any other tasks
+		}
+		 */
 		
-		if(amount<=balance){
-			System.out.println(Thread.currentThread().getName()+" is processing payment");
-			try {
-				Thread.sleep(1000);
-			}
-			catch (Exception e){
-			
-			}
-			balance-=amount;
-			System.out.println(Thread.currentThread().getName()+" completed transaction current balance "+ balance);
-			
-		}
-		else{
-			System.out.println("Insufficient balance");
-		}
 	}
 	
 	public int getBalance() {
